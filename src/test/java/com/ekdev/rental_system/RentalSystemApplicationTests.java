0d8c847376a4521058498da64d2aa3a7ffd37d96
Konrad <konradk95@gmail.com>
2025-02-09 16:17:29 +0100

    init

---
+++ src/test/java/com/ekdev/rental_system/RentalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekdev.rental_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
